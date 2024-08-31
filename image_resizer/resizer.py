62e81c023f5d0feb2e3f6886c4dfe8beccbf080c
Tim <timcansail@gmail.com>
2024-08-31 22:20:32 +1200

    remove antialiasing

--- image_resizer/resizer.py
+++ image_resizer/resizer.py
@@ -4,7 +4,7 @@ def resize_image(input_path, output_path, width):
     with Image.open(input_path) as img:
         aspect_ratio = img.height / img.width
         new_height = int(width * aspect_ratio)
-        resized_image = img.resize((width, new_height), Image.ANTIALIAS)
+        resized_image = img.resize((width, new_height))
         resized_image.save(output_path)
 
 def resize_to_single(image_path, output_path):
